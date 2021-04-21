aadae2dfa94e76520f1ae19ef6a38c2dd84d28e3
unknown <subhash227@yahoo.com>
2021-04-22 02:00:44 +1000

    footer deleted

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,12 @@
 import { AuthProvider } from "../context/AuthContext";
 import Navbar from "../components/Navbar";
 import Login from "../components/login";
-import Footer from "../components/Footer";
 const login = () => {
   return (
     <>
       <AuthProvider>
         <Navbar />
         <Login />
-        <Footer />
       </AuthProvider>
     </>
   );
