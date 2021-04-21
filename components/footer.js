e1f4b6a78be34d87a92d30789fe35c8602f651ad
unknown <subhash227@yahoo.com>
2021-04-22 01:37:57 +1000

    fragment error fixed in footer component

--- components/footer.js
+++ components/footer.js
@@ -2,18 +2,20 @@ import React from "react";
 import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="h-16 bg-blue-800 flex">
-      <p className="m-auto font-semibold text-white">
-        Сделано Субхаш Кумар Индия{" "}
-        <a
-          title="Github Link"
-          href="https://github.com/dotsimplify"
-          target="_blank"
-        >
-          <FaGithub className="inline text-3xl ml-4 hover:text-black" />
-        </a>
-      </p>
-    </div>
+    <>
+      <div className="h-16 bg-blue-800 flex">
+        <p className="m-auto font-semibold text-white">
+          Сделано Субхаш Кумар Индия{" "}
+          <a
+            title="Github Link"
+            href="https://github.com/dotsimplify"
+            target="_blank"
+          >
+            <FaGithub className="inline text-3xl ml-4 hover:text-black" />
+          </a>
+        </p>
+      </div>
+    </>
   );
 };
 
